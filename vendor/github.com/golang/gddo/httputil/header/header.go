92c29715962022a82c4aa9897b5e9fdccbff2719
Darren Shepherd <darren@rancher.com>
2016-10-09 16:57:53 -0700

    Trash updates

--- vendor/github.com/golang/gddo/httputil/header/header.go
+++ vendor/github.com/golang/gddo/httputil/header/header.go
@@ -162,7 +162,6 @@ func ParseValueAndParams(header http.Header, key string) (value string, params m
 	return
 }
 
-// AcceptSpec describes an Accept* header.
 type AcceptSpec struct {
 	Value string
 	Q     float64
@@ -281,14 +280,14 @@ func expectTokenOrQuoted(s string) (value string, rest string) {
 				case escape:
 					escape = false
 					p[j] = b
-					j++
+					j += 1
 				case b == '\\':
 					escape = true
 				case b == '"':
 					return string(p[:j]), s[i+1:]
 				default:
 					p[j] = b
-					j++
+					j += 1
 				}
 			}
 			return "", ""
